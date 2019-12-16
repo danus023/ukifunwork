8fb5563d226fcb9aa8fb90a440c42c16b296a8a7
Thayalan Danushan <thajalandanu@gmail.com>
2019-12-16 14:42:19 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+  SC.stream('/tracks/173826641', function(song) {
+    $('#p1').click(function(e) {
+      e.preventDefault();
+      song.start();
+    });
+    $('#pu1').click(function(e) {
+        e.preventDefault();
+        song.pause();
+    });
+    $('#s1').click(function(e) {
+      e.preventDefault();
+      song.stop();
+    });
+  });
+});
